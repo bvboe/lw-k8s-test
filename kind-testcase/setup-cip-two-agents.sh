a482d79a1394887887a3bd5ce9598b82a44adacb
Bjorn Boe <bvboe@pobox.com>
2021-09-14 15:45:51 -0400

    Naming conventions....

--- kind-testcase/setup-cip-two-agents.sh
+++ kind-testcase/setup-cip-two-agents.sh
@@ -4,31 +4,30 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-
 echo Creating clusters
-kind create cluster --name cip-client-twoagents
-kind create cluster --name cip-server-twoagents
+kind create cluster --name cip-twoagents-client
+kind create cluster --name cip-twoagents-server
 
 echo Deploying Lacework agent on client
-kubectl config use-context kind-cip-client-twoagents
+kubectl config use-context kind-cip-twoagents-client
 kubectl delete namespace lacework
 kubectl create namespace lacework
 kubectl config set-context --current --namespace lacework
-cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/cip-client-twoagents/" | kubectl apply -f -
+cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/cip-twoagents-client/" | kubectl apply -f -
 kubectl apply -f lacework-k8s.yaml
 kubectl config set-context --current --namespace default
 
 echo Deploying Lacework agent on server
-kubectl config use-context kind-cip-server-twoagents
+kubectl config use-context kind-cip-twoagents-server
 kubectl delete namespace lacework
 kubectl create namespace lacework
 kubectl config set-context --current --namespace lacework
-cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/cip-server-twoagents/" | kubectl apply -f -
+cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/cip-twoagents-server/" | kubectl apply -f -
 kubectl apply -f lacework-k8s.yaml
 kubectl config set-context --current --namespace default
 
 echo Deploy server apps
-kubectl config use-context kind-cip-server-twoagents
+kubectl config use-context kind-cip-twoagents-server
 kubectl config set-context --current --namespace default
 kubectl apply -f k8s-lw-proxy-test-server.yaml
 kubectl apply -f k8s-lw-proxy-test-proxy.yaml
@@ -36,10 +35,10 @@ kubectl delete svc k8s-lw-proxy-test-proxy-svc-lb k8s-lw-proxy-test-server-svc-l
 
 echo Deploy test client
 echo Getting IP for Kubernetes server
-serverIP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cip-server-twoagents-control-plane`
+serverIP=`docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cip-twoagents-server-control-plane`
 echo Server IP: $serverIP
 remoteAddress=$serverIP:30000
 echo Remote Address: $remoteAddress
-kubectl config use-context kind-cip-client-twoagents
+kubectl config use-context kind-cip-twoagents-client
 kubectl config set-context --current --namespace default
 cat k8s-lw-proxy-test-client.yaml | sed "s/insertremoteaddress/$remoteAddress/" | kubectl apply -f -
--- kind-testcase/setup-lb-one-agent.sh
+++ kind-testcase/setup-lb-one-agent.sh
@@ -4,36 +4,35 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-
 echo Creating clusters
-kind create cluster --name lb-client-oneagent
-kind create cluster --name lb-server-oneagent
+kind create cluster --name lb-oneagent-client
+kind create cluster --name lb-oneagent-server
 
 echo Deploying Lacework agent on server
-kubectl config use-context kind-lb-server-oneagent
+kubectl config use-context kind-lb-oneagent-server
 kubectl delete namespace lacework
 kubectl create namespace lacework
 kubectl config set-context --current --namespace lacework
-cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/lb-server-oneagent/" | kubectl apply -f -
+cat lacework-cfg-k8s.yaml | sed "s/insertaccesstoken/$1/" | sed "s/insertkubernetesclusterhere/lb-oneagent-server/" | kubectl apply -f -
 kubectl apply -f lacework-k8s.yaml
 kubectl config set-context --current --namespace default
 
 echo Install MetalLB
-kubectl config use-context kind-lb-server-oneagent
+kubectl config use-context kind-lb-oneagent-server
 kubectl config set-context --current --namespace default
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.10.2/manifests/metallb.yaml
 kubectl create -f metallb-conf.yaml
 
 echo Deploy server apps
-kubectl config use-context kind-lb-server-oneagent
+kubectl config use-context kind-lb-oneagent-server
 kubectl config set-context --current --namespace default
 kubectl apply -f k8s-lw-proxy-test-server.yaml
 kubectl apply -f k8s-lw-proxy-test-proxy.yaml
 kubectl delete service k8s-lw-proxy-test-proxy-svc-np
 
 echo Wait for all services to be up and running
-kubectl config use-context kind-lb-server-oneagent
+kubectl config use-context kind-lb-oneagent-server
 kubectl config set-context --current --namespace default
 kubectl get svc -A | grep pending
 while [ `kubectl get svc -A | grep pending | wc -l` -ne 0 ]
@@ -44,10 +43,10 @@ done
 
 echo Deploy test client
 echo Getting IP for Kubernetes server
-kubectl config use-context kind-lb-server-oneagent
+kubectl config use-context kind-lb-oneagent-server
 kubectl config set-context --current --namespace default
 serverIP=`kubectl get service k8s-lw-proxy-test-proxy-svc-lb -o jsonpath="{.status.loadBalancer.ingress[0].ip}"`
 echo Server IP: $serverIP
-kubectl config use-context kind-lb-client-oneagent
+kubectl config use-context kind-lb-oneagent-client
 kubectl config set-context --current --namespace default
 cat k8s-lw-proxy-test-client.yaml | sed "s/insertremoteaddress/$serverIP/" | kubectl apply -f -
